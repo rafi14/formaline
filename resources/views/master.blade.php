4e72233bc611bcac4902f7744747188329c847f9
mochammadfariz <mochammadfariz11@gmail.com>
2019-11-02 23:44:51 +0700

    first commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+     <!-- TAMBAHAN custom CSS event dan komunitas Template -->
+    <link href="css/ui.css" rel="stylesheet" type="text/css"/>
+    <link href="css/responsive.css" rel="stylesheet" media="only screen and (max-width: 1200px)" />
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  @include('layouts.includes.header')
+  
+    @yield('content')
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+     <script src="https://kit.fontawesome.com/ca9b56cb62.js"></script>
+  </body>
+
+      <!-- awal footer -->
+    <footer style="position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 70px;
+    background-color: rgb(31, 31, 31); 
+    padding-top: 20px;">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-sm-12">
+            <p style="color: #ffffff;
+font-size: 1.4em;">copy right &copy 2019. | built by. <a href="https://www.instagram.com/skyalker_/">HandsomeDev</a>.</p>
+          </div>
+        </div>
+       
+      </div>
+    </footer>
+    <!-- akhir footer -->
+</html>
---
+++ resources/views/modul/home.blade.php
@@ -0,0 +1,5 @@
+@extends('master')
+@section('content')
+test
+<i class="far fa-bell"></i>
+@endsection
